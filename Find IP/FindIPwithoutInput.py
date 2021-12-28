6bb801a823a79e8da94c781cb5580e3aa9a038f0
Maxi <50016128+MaxiAmZocken@users.noreply.github.com>
2021-12-28 19:03:22 +0100

    Create FindIPwithoutInput.py

---
+++ Find IP/FindIPwithoutInput.py
@@ -0,0 +1,13 @@
+import socket
+import os
+from requests import get
+import clipboard
+
+space = " "
+
+external_ip = get('https://api.ipify.org').content.decode('utf8')
+hostname = socket. gethostname()
+local_ip = socket. gethostbyname(hostname)
+os.system("cls")
+print("Internal IP : "+local_ip)
+print("External IP : "+external_ip)
